124b289d55a824bc5f82eb7c986a295a46b9bc7b
yshubham8419 <yshubham592@gmail.com>
2023-04-08 19:02:51 +0530

    added template

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,60 +1,37 @@
 package com.example.myapplication
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.ui.core.setContent
+import dev.playground.app.RallyApp
+import java.io.Serializable
 
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            
+        }
+    }
+}
 @Composable
-fun Downloader() {
-    var downloading by remember { mutableStateOf(false)}
-    var progress by remember { mutableStateOf(0f) }
-
-    suspend fun simulateDownload() {
-        // simulate a 10-second download process
-        repeat(10) { i ->
-            delay(1000)
-            progress = (i + 1) * 10f
-        }
-        downloading = false
-    }
-
-    suspend fun startDownload() {
-        downloading = true
-        progress = 0f
-        simulateDownload()
-    }
-
-
-
-    MaterialTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text("Downloader App") },
-                    backgroundColor = Color.Blue
-                )
-            }
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                if (downloading) {
-                    Text("Downloading... ${progress.toInt()}%")
-                    CircularProgressIndicator(progress = progress / 100f)
-                } else {
-                    Button(onClick = {  }) {
-                        Text("Download")
-                    }
-                }
-            }
-        }
-    }
+fun myApp(){
+	Column(){
+		header()
+		screen()
+		footer()
+	}
+}
+@Composable
+fun header(){
+	Text("this is header")
+}
+@Composable
+fun footer(){
+	Text("this is footer")
+}
+@Composable
+fun screen(){
+	Text("this is screen")
 }
